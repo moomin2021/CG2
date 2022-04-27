d34b58c6a8fae1c9fe31cb010386bc8cbeb41335
tamura_fumiya <k021g1134@g.neec.ac.jp>
2022-04-27 16:47:46 +0900

    Revert "03_03完了"
    This reverts commit ca76c2456d72c25749e1f4f2ec23cfc72856a286.

--- main.cpp
+++ main.cpp
@@ -236,7 +236,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 #pragma region
 
 	// 描画する頂点の数
-	int DrawVerticesNum = 4;
+	int DrawVerticesNum = 6;
 
 	// 頂点データ
 	XMFLOAT3 vertices[] =
@@ -244,7 +244,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		{ -0.5f, -0.5f, 0.0f }, // 左下
 		{ -0.5f, +0.5f, 0.0f }, // 左上
 		{ +0.5f, -0.5f, 0.0f }, // 右下
+		{ -0.5f, +0.5f, 0.0f }, // 左上
 		{ +0.5f, +0.5f, 0.0f }, // 右上
+		{ +0.5f, -0.5f, 0.0f }, // 右下
 	};
 
 	// 頂点データ全体のサイズ = 頂点データ一つ分のサイズ * 頂点データの要素数
@@ -478,13 +480,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		// キーボードで1が押されたら、三角の場合四角に切り替え、四角の場合三角に切り替える
 		if (Input::KeyTrigger(DIK_1))
 		{
-			if (DrawVerticesNum == 4)
+			if (DrawVerticesNum == 6)
 			{
 				DrawVerticesNum = 3;
 			}
 			else
 			{
-				DrawVerticesNum = 4;
+				DrawVerticesNum = 6;
 			}
 		}
 
@@ -538,7 +540,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		commandList->SetGraphicsRootSignature(rootSignature);
 
 		// プリミティブ形状の設定コマンド
-		commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP); // 三角形リスト
+		commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST); // 三角形リスト
 
 		// 頂点バッファビューの設定コマンド
 		commandList->IASetVertexBuffers(0, 1, &vbView);
