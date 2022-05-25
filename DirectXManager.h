fab3f2a8fbff14ab7c8a365e744f58350e621bd2
tamura_fumiya <k021g1134@g.neec.ac.jp>
2022-05-25 15:36:28 +0900

    DirectXManagerクラスにDrawInitialize DirectXUpdate関数を追加

--- DirectXManager.h
+++ DirectXManager.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Window.h"
 #include <d3d12.h>
 #include <dxgi1_6.h>
 #include <cassert>
@@ -24,7 +25,7 @@ public:
 	ID3D12DescriptorHeap * rtvHeap = nullptr;
 
 	// デスクリプタヒープ
-	D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc{};
+	D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc;
 
 	// バックバッファ
 	std::vector<ID3D12Resource *> backBuffers;
@@ -33,12 +34,56 @@ public:
 	ID3D12Fence * fence = nullptr;
 	UINT64 fenceVal = 0;
 
-	// コンストラクタ
+	// 頂点バッファビューの作成
+	D3D12_VERTEX_BUFFER_VIEW vbView;
+
+	// パイプランステート
+	ID3D12PipelineState * pipelineState;
+
+	// ルートシグネチャ
+	ID3D12RootSignature * rootSignature;
+
+	// インデックスバッファビューの作成
+	D3D12_INDEX_BUFFER_VIEW ibView;
+
+	ID3D12Resource * constBuffMaterial;
+
+	ID3D12DescriptorHeap * srvHeap;
+
+	ID3D12Resource * constBuffTransform;
+
+	// インデックスデータ
+	unsigned short indices[6] =
+	{
+		0, 1, 2,// 三角形1つ目
+		1, 2, 3,// 三角形2つ目
+	};
+
+public:
+
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
 	DirectXManager();
 
 	// デストラクタ
 	~DirectXManager();
 
-	// DirectXの初期化
+	/// <summary>
+	/// DirectXの初期化処理
+	/// </summary>
+	/// <param name="hwnd"> ウィンドウハンドル型 </param>
 	void DirectXInitialize(HWND & hwnd);
+
+	/// <summary>
+	/// 描画初期化処理
+	/// </summary>
+	/// <param name="winWidth"> ウィンドウ横幅 </param>
+	/// <param name="winHeight"> ウィンドウ縦幅 </param>
+	void DrawInitialize(int winWidth, int winHeight);
+
+	/// <summary>
+	/// DirectXの毎フレーム処理
+	/// </summary>
+	void DirectXUpdate(int winWidth, int winHeight);
 };
